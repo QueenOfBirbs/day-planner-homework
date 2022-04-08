145261b1520c687c5d0832ac5975dfc818a38c10
Isis Alexander <goddess.isis.alexander@gmail.com>
2022-04-07 23:44:37 -0700

    added some css

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
+// HEADER TIME 
 var current = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#pTime").text(current);
+
+// LOCAL STORAGE STUFF
+var saveButton = document.getElementById("saveButton");
+var inputBox = document.getElementById("#inputBox")
+
+function storedProject() {
+    var userInput = document.getElementById("inputBox").value;
+    localStorage.setItem(JSON.stringify(userInput));
+}
+
+function showProject() {
+    var projectText = JSON.parse(localStorage.getItem("inputBox"));
+    if (projectText !== null) {
+        document.getElementById("inputBox").innerHTML = projectText
+    }
+}
+
+// SAVE BUTTON LISTENER
+saveButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    storedProject();
+    showProject();
+});
+
+function init() {
+    showProject();
+}
+init();
