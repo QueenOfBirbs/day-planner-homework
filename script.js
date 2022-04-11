05465e7641446464d132b461378479864b68ee2a
Isis Alexander <goddess.isis.alexander@gmail.com>
2022-04-10 19:27:24 -0700

    changes

--- script.js
+++ script.js
@@ -3,29 +3,43 @@ var current = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#pTime").text(current);
 
 // LOCAL STORAGE STUFF
-var saveButton = document.getElementById("saveButton");
-var inputBox = document.getElementById("#inputBox")
+var saveButton = document.querySelectorAll(".saveButton");
+var inputBox = document.querySelectorAll(".inputBox")
 
-function storedProject() {
-    var userInput = document.getElementById("inputBox").value;
-    localStorage.setItem(JSON.stringify(userInput));
+function checkTime(){
+    var time = moment().hours()
+    for (let i = 9; i <= 17; i++) {
+        const element = array[i];
+        
+    }
+
 }
 
-function showProject() {
-    var projectText = JSON.parse(localStorage.getItem("inputBox"));
+function storedProject(number) {
+    var userInput = document.getElementById("inputBox-"+ number).value;
+    localStorage.setItem("inputBox-"+ number,JSON.stringify(userInput));
+}
+
+function showProject(number) {
+    var projectText = JSON.parse(localStorage.getItem("inputBox-"+ number));
     if (projectText !== null) {
-        document.getElementById("inputBox").innerHTML = projectText
+        document.getElementById("inputBox-"+ number).value = projectText
     }
 }
 
 // SAVE BUTTON LISTENER
-saveButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    storedProject();
-    showProject();
-});
+saveButton.forEach(function(btn){
+    btn.addEventListener("click", function (event) {
+        var number = event.target.getAttribute("id").split("-")[1]
+        event.preventDefault();
+        storedProject(number);
+        showProject(number);
+    });
+})
 
 function init() {
-    showProject();
+    for (let i = 1; i <= 9; i++) {
+        showProject(i); 
+    }
 }
 init();
